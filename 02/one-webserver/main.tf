a3041273a20a6e97a20bf9ca732a0e6e7aebe345
jang4sc <jang4sc@hanmail.net>
2025-12-11 16:33:35 +0900

    변경사항

--- 02/one-webserver/main.tf
+++ 02/one-webserver/main.tf
@@ -47,8 +47,8 @@ resource "aws_vpc_security_group_ingress_rule" "allow_8080_http" {
   security_group_id = aws_security_group.allow_8080.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8080
-  ip_protocol       = "tcp"
   to_port           = 8080
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
